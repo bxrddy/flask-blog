f1138c1b3fcc0e897b516be5d4065a14b576491b
Brijesh <brijesh.reddy15@siesgst.ac.in>
2020-02-16 02:07:22 +0530

    User Authentication completed

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from flaskblog.models import User
 
 
 # Registration Form
@@ -11,6 +12,16 @@ class RegistrationForm(FlaskForm):
 	password 		  =  PasswordField('Password', validators = [DataRequired()])
 	confirm_password  =  PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
 	submit 			  =  SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username = username.data).first()
+		if user:
+			raise ValidationError('This username is already taken. Please choose a different one.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email = email.data).first()
+		if user:
+			raise ValidationError('This email is already taken. Please choose a different one.')
 
 # Login Form
 class LoginForm(FlaskForm):
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -39,11 +40,21 @@ def about():
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
 
+    # If user is already logged in then redirect the user to the homepage
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")   # Used the Bootstrap 'success' class as a category for flash
-        return redirect(url_for('home'))
+
+        # decode('utf-8') is used to convert the hashed password from byte to string
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your account has been created. You can now login!", "success")   # Used the Bootstrap 'success' class as a category for flash
+        return redirect(url_for('login'))
 
     return render_template('register.html', title = 'Register', form = form)
 
@@ -51,13 +62,37 @@ def register():
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
 
+    # If user is already logged in then redirect the user to the homepage
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        if form.email.data == 'abc@gmail.com' and form.password.data == '123':
-            flash(f"You are now logged in!", "success")   # Used the Bootstrap 'success' class as a category for flash
-            return redirect(url_for('home'))
+
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+
+            # Stores the information of the URL user tried to access before authentication
+            next_page = request.args.get('next')
+
+            # Redirects the user to the desired URL after authentication
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash(f"Login failed. Please check your credentials!", "danger")
+            flash("Login failed. Please check your credentials!", "danger")
 
     return render_template('login.html', title = 'Login', form = form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("You are now logged out!", "warning")
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title = 'Account')
